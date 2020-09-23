02db185ae400b7c4da9f717f5971b627795caec7
Elio-yang <2925539854@qq.com>
2020-09-23 15:41:46 +0800

    bug fixed

--- hash.h
+++ hash.h
@@ -1,41 +1,41 @@
 #ifndef _HASH_H_
-
+#define _HASH_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
+#define ERROR_SPACE() printf("Out of space,%d,%s\n",__LINE__,__func__)
 /*key 的类型*/
 #define data_t char*
 /*哈希表的最小大小*/
 #define MIN_SIZE 29
 /*散列表元素链表的节点*/
-struct node{
-    data_t data;
-    struct node *next;
+struct node {
+	data_t data;
+	struct node *next;
 };
 
 /*分离链接散列表*/
-struct hash_table{
-    int table_size;
-    /*
-     * 指针数组：
-     * 第一次malloc：H->list=(struct node**)malloc(sizeof(struct node*)*H->table_size)
-     * 第二次malloc：H->list[i]=（struct node*)malloc(sizeof(struct node))
-     * */
-    struct node* *list;
+struct hash_table {
+	int table_size;
+	/*
+	 * 指针数组：
+	 * 第一次malloc：H->list=(struct node**)malloc(sizeof(struct node*)*H->table_size)
+	 * 第二次malloc：H->list[i]=（struct node*)malloc(sizeof(struct node))
+	 * */
+	struct node **list;
 };
 
 /*hash函数*/
-extern unsigned int hash(const char *str,int table_size);
+extern unsigned int hash(const char *str, int table_size);
 /*初始化哈希表*/
-extern struct hash_table * init_hash(int table_size);
+extern struct hash_table *init_hash(int table_size);
 extern void destory_hash(struct hash_table *h);
-extern struct node *contain(data_t key,struct hash_table *h);
+extern struct node *contain(data_t key, struct hash_table *h);
 /*解决冲突：分离链接法  插入*/
-extern void put(data_t key,struct hash_table *h);
+extern void put(data_t key, struct hash_table *h);
 /*生成下一个素数*/
 extern int next_prime(int n);
 extern int is_prime(int n);
-/**/
+
 #endif
--- hash_test.c
+++ hash_test.c
@@ -1,8 +1,19 @@
 #include "hash.h"
+#include "hash.c"
+
+extern struct hash_table *init_hash(int table_size);
+extern void put(data_t key, struct hash_table *h);
 int main()
 {
-    struct hash_table * H;
-    H=init_hash(1000);
-    put("hello",H);
-    return 0;
+	struct hash_table *H;
+	H = init_hash(400);
+	put("hello", H);
+	if(contain("hello",H)!=NULL){
+		printf("exist \"hello\" \n");
+	}
+	put("test",H);
+	if(contain("test",H)!=NULL){
+		printf("exist \"test\" \n");
+	}
+	return 0;
 }
