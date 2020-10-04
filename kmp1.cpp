eb693d9bcc03df187668eea29577cf967ebbd91b
Elio-yang <2925539854@qq.com>
2020-10-04 11:50:20 +0800

    kmp

--- kmp1.cpp
+++ kmp1.cpp
@@ -3,28 +3,52 @@
 #include <cstring>
 using namespace std;
 
+#define maxn 100010
+
 int* build_next(char *P);
-int kmp(char *T,char *P);
+int* build_next_improve(char *P);
+int KMP(char *T,char *P);
+int* build_fail(char *p);
+
 int main()
 {
-
-    while (1)
-    {
-        char str[100];
-        scanf("%s",str);
-        int *next=build_next(str);
-        int m=strlen(str);
-        for(int i=0;i<m;i++)
-        {
-            cout<<next[i]<<" ";
-        }
+    char txt[maxn];
+    char pat[maxn];
+    scanf("%s",txt);
+    scanf("%s",pat);
+    int len=strlen(pat);
+/*===========================================================================*/
+    int *fail=build_fail(pat);
+    cout<<"fail_table: ";
+    for(int i=0;i<len;i++){
+        cout<<fail[i]<<" ";
     }
+    cout<<endl;
+/*===========================================================================*/
+    cout<<"next table :";
+    int *next=build_next(pat);
+    for(int i=0;i<len;i++){
+        cout<<next[i]<<" ";
+    }
+    cout<<endl;
+/*===========================================================================*/
+    cout<<"next table 2 :";
+    int *next2=build_next_improve(pat);
+    for(int i=0;i<len;i++){
+        cout<<next2[i]<<" ";
+    }
+    cout<<endl;
+/*===========================================================================*/
+
+    int pos=KMP(txt,pat);
+    cout<<pos<<endl;
+
     system("pause");
     return 0;
 }
-int* build_next(char *P)
+int *build_next(char *P)
 {
-    size_t m=strlen(P),j=0;
+    int m=(int)strlen(P),j=0;
     int *N=new int[m];
     int t=N[0]=-1;
     while(j<m-1){
@@ -38,23 +62,61 @@ int* build_next(char *P)
     }
     return N;
 }
-int kmp(char *T,char *P)
+int *build_next_improve(char *P)
 {
-    int *next=build_next(P);
     int m=(int)strlen(P),j=0;
-    int n=(int)strlen(T),i=0;
-    while(j<m&&i<n){
-        if(0>j||P[j]==T[i]){
-            i++;
+    int *N=new int[m];
+    int t=N[0]=-1;
+    while(j<m-1){
+        if(0>t||P[j]==P[t]){
             j++;
+            t++;
+            N[j]=(P[t]!=P[j])?t:N[t];
         }else{
-            j=next[j];
+            t=N[t];
         }
     }
-    for(int k=0;k<m;k++){
-        cout<<next[k]<<" ";
+    return N;
+}
+int *build_fail(char *P){
+    int len=(int)strlen(P);
+    int* F=new int[len];
+    F[0]=-1;
+    for(int j=1;j<len;j++){
+        int i=F[j-1];
+        while(P[j]!=P[i+1]&&i>=0) {
+            i=F[i] ;
+        }  
+        if(P[j]==P[i+1]){
+            F[j]=i+1;  
+        }
+        else {
+            F[j]=-1;
+        }
     }
-    cout<<endl;
-    delete[] next;
-    return i-j;
+    return F;
+}
+int KMP(char * T, char * P) 
+{
+	int i = 0; 
+	int j = 0;
+    int m=(int)strlen(P);
+    int n=(int)strlen(T);
+    int *next=build_next_improve(P);
+	while (i < n && j < m)
+	{
+		if (0 > j || T[i] == P[j]){
+			i++;
+           	j++;
+		}
+	 	else{
+           	j = next[j];
+    	}
+    }
+    if (j == m){
+       return i - j;
+    }
+    else {
+       return -1;
+    }
 }
